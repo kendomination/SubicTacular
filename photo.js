62492fcabdabb68713aefde1c7dde598ced602cd
kendomination <kennethcanlas630@gmail.com>
2013-12-22 03:37:28 +0800

    Android Version

---
+++ photo.js
@@ -0,0 +1,79 @@
+jQuery(function($) {
+                $("#old").click(function(e){
+                                e.preventDefault();
+                                $.swipebox([
+                                        { href : 'images/old subic/1.jpg', title : 'Subic from the Past' },
+                                        { href : 'images/old subic/2.jpg', title : 'Subic from the Past' },
+                                        { href : 'images/old subic/3.jpg', title : 'Subic from the Past' },
+                                        { href : 'images/old subic/4.jpg', title : 'Subic from the Past' },
+                                        { href : 'images/old subic/5.jpg', title : 'Subic from the Past' },
+                                        { href : 'images/old subic/6.jpg', title : 'Subic from the Past' },
+                                        { href : 'images/old subic/7.jpg', title : 'Subic from the Past' },
+                                        { href : 'images/old subic/8.jpg', title : 'Subic from the Past' },
+                                        { href : 'images/old subic/9.jpg', title : 'Subic from the Past' }
+                        
+                                ]);
+                        });
+                        $("#town").click(function(e){
+                                e.preventDefault();
+                                $.swipebox([
+                                        { href : 'images/town/1.jpg', title : 'Around the Town' },
+                                        { href : 'images/town/2.jpg', title : 'Around the Town' },
+                                        { href : 'images/town/3.jpg', title : 'Around the Town' },
+                                        { href : 'images/town/4.jpg', title : 'Around the Town' },
+                                        { href : 'images/town/5.jpg', title : 'Around the Town' },
+                                        { href : 'images/town/6.jpg', title : 'Around the Town' },
+                                        { href : 'images/town/7.jpg', title : 'Around the Town' },
+                                        { href : 'images/town/8.jpg', title : 'Around the Town' },
+                                        { href : 'images/town/9.jpg', title : 'Around the Town' },
+                                        { href : 'images/town/10.jpg', title : 'Around the Town' },
+                                        { href : 'images/town/11.jpg', title : 'Around the Town' }
+                                   
+                        
+                        
+                                ]);
+                        });
+                                $("#where").click(function(e){
+                                e.preventDefault();
+                                $.swipebox([
+                                        { href : 'images/subic/1.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/2.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/3.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/4.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/5.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/6.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/7.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/8.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/9.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/10.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/11.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/12.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/13.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/14.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/15.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/16.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/17.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/18.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/19.jpg', title : 'Where Mountains Meet the Sea' },
+                                        { href : 'images/subic/20.jpg', title : 'Where Mountains Meet the Sea' }
+                                 
+                        
+                                ]);
+                        });
+						 $("#fish").click(function(e){
+                                e.preventDefault();
+                                $.swipebox([
+                                        { href : 'images/fishing/1.jpg', title : 'The Fishing Industry in Subic' },
+                                        { href : 'images/fishing/2.jpg', title : 'The Fishing Industry in Subic' },
+                                        { href : 'images/fishing/3.jpg', title : 'The Fishing Industry in Subic' },
+                                        { href : 'images/fishing/4.jpg', title : 'The Fishing Industry in Subic' },
+                                        { href : 'images/fishing/5.jpg', title : 'The Fishing Industry in Subic' },
+                                        { href : 'images/fishing/6.jpg', title : 'The Fishing Industry in Subic' },
+                                        { href : 'images/fishing/7.jpg', title : 'The Fishing Industry in Subic' },
+                                        { href : 'images/fishing/8.jpg', title : 'The Fishing Industry in Subic' },
+                                        { href : 'images/fishing/9.jpg', title : 'The Fishing Industry in Subic' },
+                                        { href : 'images/fishing/10.jpg', title : 'The Fishing Industry in Subic' },
+                                        { href : 'images/fishing/11.jpg', title : 'The Fishing Industry in Subic' }  
+                                ]);
+                        });
+                   });    
